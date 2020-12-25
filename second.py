70493f068f790a2f9dab19cacf396f96f7346bd5
shaoyulin <75822327+shaoyulin@users.noreply.github.com>
2020-12-25 16:10:12 +0800

    Update second.py

--- second.py
+++ second.py
@@ -1,1 +1,55 @@
 # second page
+import csv
+
+class Restaurant:
+    def __init__ (self, name, meal, locate):
+        self.name = name
+        self.meal = meal
+        self.locate = locate
+
+        
+    def check_meal(self):
+        meallist = self.meal.split(', ')
+        cnt = 0
+        for meal in meallist:
+            cnt += 1
+            if meal in choose_meal:
+                return True
+                break
+
+            if cnt == len(meallist):
+                return False
+
+    def check_locate(self):
+        if self.locate in choose_locate:
+            return True
+        else:
+            return False
+
+with open('canteen.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    name2locate = dict()
+    name2meal = dict()
+    name2style = dict()
+    name2phone = dict()
+    name2address = dict()
+
+    choose_locate = input()
+    choose_meal = input()
+
+    for row in reader:
+        row.pop(0)
+        # print(row)
+        name2locate[row[0]] = row[1]
+        name2meal[row[0]] = row[2]
+        name2style[row[0]] = row[3]
+        name2phone[row[0]] = row[4]
+        name2address[row[0]] = row[5]
+
+        name = row[0]
+        meal = row[2]
+        locate = row[1]
+        res = Restaurant(name, meal, locate)
+        # print(res.check_locate())
+        # print(res.check_meal())
+    f.close
