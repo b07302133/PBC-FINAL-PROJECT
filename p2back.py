95ff94a5880279cae0fcdb4691b23b93c7ecc6ee
HPW610 <76542340+HPW610@users.noreply.github.com>
2020-12-27 03:46:34 +0800

    FIXED_bug_in_check_day
    解決check_day的bug了

--- p2back.py
+++ p2back.py
@@ -6,48 +6,6 @@
 
 import csv
 
-class Restaurant:
-    def __init__ (self, name, meal, locate, style, day, day2open, star):
-        self.name = name  # 餐廳名稱
-        self.meal = meal  # 早中晚宵
-        self.locate = locate  # 地點
-        self.style = style  # 風格(Ex.日式)
-        # 博文：新加了兩個
-        self.day = day  # 輸入之日期
-        self.day2open = day2open  # 日期對應開放時間的dictionary
-        self.star = star
-
-    def check_meal(self):  # 所選時段餐廳有沒有開的布林
-        meallist = self.meal.split(', ')  # 預設為字串，前端用list設定選擇時段的話：可刪!!
-        cnt = 0
-        for meal in meallist:
-            cnt += 1
-            if meal in choose_meal:
-                return True  # 選擇的任一時段餐廳有開的話即回傳True
-                break
-            if cnt == len(meallist):
-                return False
-
-    def check_locate(self):  # 選擇地點並讓其回傳布林
-        if self.locate in choose_locate:
-            return True
-        else:
-            return False
-    
-    def check_day(self):  # 回傳選擇的日期有沒有開的布林
-        cnt = 0
-        if self.day in self.day2open.keys():
-            if self.day2open[self.day] == '':
-                return False
-            else:
-                return True
-
-    def check_style(self):  # 選擇風格並回傳布林
-    	if self.style in choose_style:
-    		return True
-    	else:
-    		return False
-
 def choose_style_sorted(choose_style):  # 為了演算法而讓每個類型有個分數
     list_style = choose_style.split(',')
     list_sequence_point = []
@@ -71,6 +29,52 @@ def recommendation(restaurant_dict):
 
     return recommendation_list
 
+
+class Restaurant:
+    def __init__ (self, name, meal, locate, style, day, name2open, star):
+        self.name = name  # 餐廳名稱
+        self.meal = meal  # 早中晚宵
+        self.locate = locate  # 地點
+        self.style = style  # 風格(Ex.日式)
+        # 博文：新加了兩個
+        self.day = day  # 輸入之日期
+        self.name2open = name2open  # 日期對應開放時間的dictionary
+        self.star = star
+    
+    def put_name(self):  # call餐廳名稱
+        return self.name
+
+    def check_meal(self):  # 所選時段餐廳有沒有開的布林
+        meallist = self.meal.split(', ')  # 預設為字串，前端用list設定選擇時段的話：可刪!!
+        cnt = 0
+        for meal in meallist:
+            cnt += 1
+            if meal in choose_meal:
+                return True  # 選擇的任一時段餐廳有開的話即回傳True
+                break
+            if cnt == len(meallist):
+                return False
+
+    def check_locate(self):  # 選擇地點並讓其回傳布林
+        if self.locate in choose_locate:
+            return True
+        else:
+            return False
+    
+    def check_day(self):  # 回傳選擇的日期有沒有開的布林
+        cnt = 0
+        weeklist = ['MON','TUE','WED','THU','FRI','SAT','SUN']
+        if self.name2open[self.name][weeklist.index(self.day)]== '':
+            return False
+        else:
+            return True
+
+    def check_style(self):  # 選擇風格並回傳布林
+        if self.style in choose_style:
+            return True
+        else:
+            return False
+
 with open('canteen.csv', 'r', encoding='utf-8') as f:  # 讀csv檔
     # 製作各種字典（除星期），之後可能會用到
     reader = csv.reader(f)
@@ -86,9 +90,10 @@ with open('canteen.csv', 'r', encoding='utf-8') as f:  # 讀csv檔
     choose_meal = input()
     choose_style = input()
     day = input() # 輸出的日期
-    restaurant_dict = dict()  # 要處理的dict
     
-    search_list = []  # 點好後輸出的清單在這!!（博文、前端）
+    restaurant_dict = dict()  # 點好後輸出的清單在這!!（博文、前端）
+    
+    name2open = dict()  # 博文、振安：跑check_day的dictionary
     for row in reader:  # 跑csv檔的每一行
         row.pop(0)
         # print(row)
@@ -98,16 +103,7 @@ with open('canteen.csv', 'r', encoding='utf-8') as f:  # 讀csv檔
         name2phone[row[0]] = row[4]
         name2address[row[0]] = row[5]
         name2stars[row[0]] = row[6]
-        
-        # 製作字典，前端注意：日期的botton麻煩按照下面的打法打(例:MON, TUE...)
-        day2open = dict()
-        day2open['MON'] = row[8]
-        day2open['TUE'] = row[10]
-        day2open['WED'] = row[12]
-        day2open['THU'] = row[14]
-        day2open['FRI'] = row[16]
-        day2open['SAT'] = row[18]
-        day2open['SUN'] = row[20]
+        name2open[row[0]] = [row[8],row[10],row[12],row[14],row[16],row[18],row[20]]  # 博文、振安：跑check_day的dictionary
         
         # 要class所需要的值
         name = row[0]
@@ -115,20 +111,20 @@ with open('canteen.csv', 'r', encoding='utf-8') as f:  # 讀csv檔
         meal = row[2]
         style = row[3]
         star = row[6]
-        res = Restaurant(name, meal, locate, style, day, day2open, star)  # 開始使用class Restaurant
+        res = Restaurant(name, meal, locate, style, day, name2open, star)  # 開始使用class Restaurant
         # print(res.check_locate())
         # print(res.check_meal())
         # print(res.check_style())
         # print(res.check_day())
         
         # 用布林篩選輸出的清單
-        # check_day有bug
-        print(res.check_day())
         if res.check_locate() == True:
             if res.check_meal() == True:
-                for style_ in choose_style_sorted(choose_style):
-                    if res.style in style_:
-                        restaurant_dict[res.name] = [res.locate, res.meal, res.style, res.star, style_[1]]
+                if res.check_day() == True:
+                    if res.check_style() == True:
+                        for style_ in choose_style_sorted(choose_style):
+                            if res.style in style_:
+                                restaurant_dict[res.name] = [res.locate, res.meal, res.style, res.star, style_[1]]                        
 
 print(recommendation(restaurant_dict))
 
