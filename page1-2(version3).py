418068845e2c5a245da04c12d18e59c421ffa07f
ahn0209 <55389739+ahn0209@users.noreply.github.com>
2020-12-28 02:15:13 +0800

    新增了第三第四頁的畫面

--- page1-2(version3).py
+++ page1-2(version3).py
@@ -6,6 +6,7 @@ import csv
 """
 all_condition_list = []
 recommendation_res_list = []
+res_name = ""
 restaurant_dict = dict()
 # 將視窗作為一個物件
 class window(Tk):
@@ -511,30 +512,29 @@ class Restaurant:
             return False
 
 class PageThree(Frame):    
-    def get_res():
-        res_name = var.get()
+    def get_res(self):
+        res_name = self.res_var.get()
     def __init__(self, master):
         Frame.__init__(self, master)
-        res_name = ""
+        global res_name
         self.res_var = StringVar()
         self.no1_option = Radiobutton(self, width = 20, height = 4, text="第一推薦："+ \
-                          recommendation_res_list[0][0], variable = self.res_var, value = recommendation_res_list[0][0], command=lambda:[get_res(),master.switch_frame(PageFour)])
+                          recommendation_res_list[0][0], variable = self.res_var, value = recommendation_res_list[0][0], command=lambda:[self.get_res(),master.switch_frame(PageFour)],indicatoron = 0)
         self.no2_option = Radiobutton(self, width = 20, height = 4, text="第二推薦："+ \
-                          recommendation_res_list[1][0], variable = self.res_var, value = recommendation_res_list[1][0], command=lambda:[get_res(),master.switch_frame(PageFour)])
+                          recommendation_res_list[1][0], variable = self.res_var, value = recommendation_res_list[1][0], command=lambda:[self.get_res(),master.switch_frame(PageFour)],indicatoron = 0)
         self.no3_option = Radiobutton(self, width = 20, height = 4, text="第三推薦："+ \
-                          recommendation_res_list[2][0], variable = self.res_var, value = recommendation_res_list[2][0], command=lambda:[get_res(),master.switch_frame(PageFour)])
+                          recommendation_res_list[2][0], variable = self.res_var, value = recommendation_res_list[2][0], command=lambda:[self.get_res(),master.switch_frame(PageFour)],indicatoron = 0)
         self.no4_option = Radiobutton(self, width = 20, height = 4, text="第四推薦："+ \
-                          recommendation_res_list[3][0], variable = self.res_var, value = recommendation_res_list[3][0], command=lambda:[get_res(),master.switch_frame(PageFour)])
+                          recommendation_res_list[3][0], variable = self.res_var, value = recommendation_res_list[3][0], command=lambda:[self.get_res(),master.switch_frame(PageFour)],indicatoron = 0)
         self.no5_option = Radiobutton(self, width = 20, height = 4, text="第五推薦："+ \
-                          recommendation_res_list[4][0], variable = self.res_var, value = recommendation_res_list[4][0], command=lambda:[get_res(),master.switch_frame(PageFour)])
-        self.no1_option.grid(row = 0, column = 1, sticky = 'n'+'s', pady = 5)
-        self.no2_option.grid(row = 1, column = 1, sticky = 'n'+'s', pady = 5)
-        self.no3_option.grid(row = 2, column = 1, sticky = 'n'+'s', pady = 5)
-        self.no4_option.grid(row = 3, column = 1, sticky = 'n'+'s', pady = 5)
-        self.no5_option.grid(row = 4, column = 1, sticky = 'n'+'s', pady = 5)
+                          recommendation_res_list[4][0], variable = self.res_var, value = recommendation_res_list[4][0], command=lambda:[self.get_res(),master.switch_frame(PageFour)],indicatoron = 0)
+        self.no1_option.pack(side="top")
+        self.no2_option.pack(side="top")
+        self.no3_option.pack(side="top")
+        self.no4_option.pack(side="top")
+        self.no5_option.pack(side="top")
 
 class PageFour(Frame):    
-    
     def __init__(self, master):
         Frame.__init__(self, master)
         # 上一步按鍵
@@ -544,23 +544,23 @@ class PageFour(Frame):
         self.restart_btn = Button(text = "重來", bg = "white", width=8, height=2,\
         command=lambda: master.switch_frame(StartPage)).grid(row = 7, column = 2)
         #餐廳名稱
-        self.res_name_label = Label(text= res.name + "的資訊", bg = "skyblue")
+        self.res_name_label = Label(text= res_name + "的資訊", bg = "skyblue")
         self.res_name_label.config(height=3,font ="微軟正黑體 20")
         self.res_name_label.pack(side="top")
         # 餐廳地址
-        self.res_address_label = Label(text="地址："+ name2address[res.name], bg = "skyblue")
+        self.res_address_label = Label(text="地址："+ name2address[res_name], bg = "skyblue")
         self.res_address_label.config(height=3,font ="微軟正黑體 20")
         self.res_address_label.pack(side="top")
         # 餐廳電話
-        self.res_phone_label = Label(text= "電話："+name2phone[res.name], bg = "skyblue")
+        self.res_phone_label = Label(text= "電話："+name2phone[res_name], bg = "skyblue")
         self.res_phone_label.config(height=3, font ="微軟正黑體 20")
         self.res_phone_label.pack(side="top")
         # 餐廳營業時間
-        self.res_time_label = Label(text="營業時間："+name2open[res.name], bg = "skyblue")
+        self.res_time_label = Label(text="營業時間："+name2open[res_name], bg = "skyblue")
         self.res_time_label.config(height=3, font ="微軟正黑體 20")
         self.res_time_label.pack(side="top")
         # 餐廳星級
-        self.res_star_label = Label(text="星級："+ name2stars[res.name], bg = "skyblue")
+        self.res_star_label = Label(text="星級："+ name2stars[res_name], bg = "skyblue")
         self.res_star_label.config(height=3, font ="微軟正黑體 20")
         self.res_star_label.pack(side="top")
         # 時間提醒
