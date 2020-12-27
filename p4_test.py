4876efee5b4c8bd4035846ad9a3a4b63a49f99fe
ahn0209 <55389739+ahn0209@users.noreply.github.com>
2020-12-27 14:56:01 +0800

    Create p4_test.py

---
+++ p4_test.py
@@ -0,0 +1,30 @@
+class PageFour(Frame):    
+    
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        # 上一步按鍵
+        self.back_btn = Button(self, text="回到上一頁", width=8, height=2, bg = "white",\
+        command=lambda: master.switch_frame(PageThree)).grid(row = 6, column = 2)
+        # 重來按鍵
+        self.restart_btn = Button(text = "重來", bg = "white", width=8, height=2,\
+        command=lambda: master.switch_frame(StartPage)).grid(row = 7, column = 2)
+        #餐廳名稱
+        res_name_label = Label(text= res.name + "的資訊", bg = "skyblue")
+        res_name_label.config(height=3,font ="微軟正黑體 20")
+        res_name_label.pack(side="top")
+        # 餐廳地址
+        res_address_label = Label(text="地址："+ name2address[res.name], bg = "skyblue")
+        res_address_label.config(height=3,font ="微軟正黑體 20")
+        res_address_label.pack(side="top")
+        # 餐廳電話
+        res_phone_label = Label(text= "電話："+name2phone[res.name], bg = "skyblue")
+        res_phone_label.config(height=3, font ="微軟正黑體 20")
+        res_phone_label.pack(side="top")
+        # 餐廳營業時間
+        res_time_label = Label(text="營業時間："+name2open[res.name], bg = "skyblue")
+        res_time_label.config(height=3, font ="微軟正黑體 20")
+        res_time_label.pack(side="top")
+        # 餐廳星級
+        res_star_label = Label(text="星級："+ name2stars[res.name], bg = "skyblue")
+        res_star_label.config(height=3, font ="微軟正黑體 20")
+        res_star_label.pack(side="top")
