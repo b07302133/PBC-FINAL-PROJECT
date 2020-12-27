2b7940b6b2115fe5d76435f77027ecca9ffb7c04
ahn0209 <55389739+ahn0209@users.noreply.github.com>
2020-12-27 15:03:38 +0800

    Update p4_test.py

--- p4_test.py
+++ p4_test.py
@@ -9,22 +9,24 @@ class PageFour(Frame):
         self.restart_btn = Button(text = "重來", bg = "white", width=8, height=2,\
         command=lambda: master.switch_frame(StartPage)).grid(row = 7, column = 2)
         #餐廳名稱
-        res_name_label = Label(text= res.name + "的資訊", bg = "skyblue")
-        res_name_label.config(height=3,font ="微軟正黑體 20")
-        res_name_label.pack(side="top")
+        self.res_name_label = Label(text= res.name + "的資訊", bg = "skyblue")
+        self.res_name_label.config(height=3,font ="微軟正黑體 20")
+        self.res_name_label.pack(side="top")
         # 餐廳地址
-        res_address_label = Label(text="地址："+ name2address[res.name], bg = "skyblue")
-        res_address_label.config(height=3,font ="微軟正黑體 20")
-        res_address_label.pack(side="top")
+        self.res_address_label = Label(text="地址："+ name2address[res.name], bg = "skyblue")
+        self.res_address_label.config(height=3,font ="微軟正黑體 20")
+        self.res_address_label.pack(side="top")
         # 餐廳電話
-        res_phone_label = Label(text= "電話："+name2phone[res.name], bg = "skyblue")
-        res_phone_label.config(height=3, font ="微軟正黑體 20")
-        res_phone_label.pack(side="top")
+        self.res_phone_label = Label(text= "電話："+name2phone[res.name], bg = "skyblue")
+        self.res_phone_label.config(height=3, font ="微軟正黑體 20")
+        self.res_phone_label.pack(side="top")
         # 餐廳營業時間
-        res_time_label = Label(text="營業時間："+name2open[res.name], bg = "skyblue")
-        res_time_label.config(height=3, font ="微軟正黑體 20")
-        res_time_label.pack(side="top")
+        self.res_time_label = Label(text="營業時間："+name2open[res.name], bg = "skyblue")
+        self.res_time_label.config(height=3, font ="微軟正黑體 20")
+        self.res_time_label.pack(side="top")
         # 餐廳星級
-        res_star_label = Label(text="星級："+ name2stars[res.name], bg = "skyblue")
-        res_star_label.config(height=3, font ="微軟正黑體 20")
-        res_star_label.pack(side="top")
+        self.res_star_label = Label(text="星級："+ name2stars[res.name], bg = "skyblue")
+        self.res_star_label.config(height=3, font ="微軟正黑體 20")
+        self.res_star_label.pack(side="top")
+        # 時間提醒
+        self.info_label = Label(text="", bg = "white", width=8, height=2)
