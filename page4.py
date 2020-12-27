1a09f7c19229aad96b3d7358fce796c16c8fe259
ahn0209 <55389739+ahn0209@users.noreply.github.com>
2020-12-27 14:37:01 +0800

    修正變數

--- page4.py
+++ page4.py
@@ -5,29 +5,23 @@ page_4.geometry("800x600")
 page_4.config(background="skyblue")  # 背景顏色
 page_4.resizable(False, False)  # 固定視窗大小
 
-#res.name = "上海小館"
-#res.address = "新北市永和區永平路252號"
-#res.phone = "02-2925-3608"
-#res.star = "4.6"
-#res.time = "11:00~15:00, 17:30~21:00"
-
-res_name_label = Label(text= "上海小館" + "的資訊", bg = "skyblue")
+res_name_label = Label(text= res.name + "的資訊", bg = "skyblue")
 res_name_label.config(height=3,font ="微軟正黑體 20")
 res_name_label.pack(side="top")
 
-res_address_label = Label(text="地址："+"新北市永和區永平路252號", bg = "skyblue")
+res_address_label = Label(text="地址："+ name2address[res.name], bg = "skyblue")
 res_address_label.config(height=3,font ="微軟正黑體 20")
 res_address_label.pack(side="top")
 
-res_phone_label = Label(text= "電話："+"02-2925-3608", bg = "skyblue")
+res_phone_label = Label(text= "電話："+name2phone[res.name], bg = "skyblue")
 res_phone_label.config(height=3, font ="微軟正黑體 20")
 res_phone_label.pack(side="top")
 
-res_time_label = Label(text="營業時間："+"11:00~15:00, 17:30~21:00", bg = "skyblue")
+res_time_label = Label(text="營業時間："+name2open[res.name], bg = "skyblue")
 res_time_label.config(height=3, font ="微軟正黑體 20")
 res_time_label.pack(side="top")
 
-res_star_label = Label(text="星級："+"4.6", bg = "skyblue")
+res_star_label = Label(text="星級："+ name2stars[res.name], bg = "skyblue")
 res_star_label.config(height=3, font ="微軟正黑體 20")
 res_star_label.pack(side="top")
 
